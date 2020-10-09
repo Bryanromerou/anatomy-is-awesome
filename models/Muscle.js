18645002b48a6d4491f3c78574918c642a3c0139
Bryanromerou <Bryanromerou@gmail.com>
2020-10-08 23:00:34 -0700

    added basic routes to Body model

--- models/Muscle.js
+++ models/Muscle.js
@@ -3,6 +3,14 @@ const Schema = mongoose.Schema;
 
 const muscleSchema = new Schema({
     name: {type: String, required: true},
+    body: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Body",
+    },
+    // bones:{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "Muscle"
+    // },
 },{timestamps: true});
 
 const Muscle = mongoose.model("Muscle", muscleSchema);
